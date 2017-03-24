fd3f568508eea41dd868cbc0f3567ec1ab86d7a4
aran.dehkharghani <aran.dehkharghani@gmail.com>
2017-03-24 23:27:14 +1100

    added cloud mongodb config

--- api/shared/app-settings.ts
+++ api/shared/app-settings.ts
@@ -15,6 +15,9 @@ export class Config {
                 gateway_base_path: 'http://localhost:10020',
                 strategy_base_path: 'http://localhost:10010',
                 ui_base_path: 'http://localhost:8080',
+                mongo_db_connection_string: `mongodb://tforex-user:tforex-password
+                @cluster0-shard-00-00-tyqk3.mongodb.net:27017,cluster0-shard-00-01-tyqk3.mongodb.net:27017,
+                cluster0-shard-00-02-tyqk3.mongodb.net:27017/tforex?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin`,
                 api_key: '1234',
                 kafka_conn_string: 'localhost:2181/',
                 oanda_access_token_key: '77b8d34f242ab412698eba34bc577edb-9126983f28bbf9348c2e2f5697c9d1b3',
@@ -38,6 +41,7 @@ interface AppSettings {
     strategy_base_path: string;
     ui_base_path: string;
     api_key: string;
+    mongo_db_connection_string: string;
     kafka_conn_string: string;
     oanda_access_token_key: string;
     candle_history_client_id: string;
--- api/shared/data-access.ts
+++ api/shared/data-access.ts
@@ -1,5 +1,7 @@
 import { Mongoose } from "mongoose";
 
+import * as api from '../shared';
+
 export class DataAccess {
     public static mongooseInstance: Mongoose;
 
@@ -14,7 +16,7 @@ export class DataAccess {
         this.mongooseInstance.connection.once("open", () => {
             console.log("Conected to mongodb.");
         });
-        this.mongooseInstance.connect('mongodb://localhost/tforex');
+        this.mongooseInstance.connect(api.Config.settings.oanda_access_token_key);
         return this.mongooseInstance;
     }
 
