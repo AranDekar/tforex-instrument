ec0bbdae267988a4e47b26e7d0fc26fa6ea6b74e
aran.dehkharghani <aran.dehkharghani@gmail.com>
2017-05-08 23:19:12 +1000

    added history data to candle controller

--- api/candle/controllers/candle.controller.ts
+++ api/candle/controllers/candle.controller.ts
@@ -55,3 +55,22 @@ export async function importCandles(req, res, next) {
         next(err);
     }
 }
+
+export async function getHistoryData(req, res, next) {
+    try {
+        let instrument: shared.InstrumentEnum;
+        let granularity: shared.GranularityEnum;
+        let topic: string;
+
+        topic = req.swagger.params.topic.value;
+        instrument = req.swagger.params.instrument.value;
+        granularity = req.swagger.params.granularity.value;
+
+        let service = new api.Service.CandleService();
+        await service.getHistoryData(topic, instrument, granularity);
+        res.status(200).json({ message: 'candles are being published' });
+    } catch (err) {
+        res.statusCode = 500; // bad server
+        next(err);
+    }
+}
