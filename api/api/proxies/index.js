6c4cd20231ef12793d8f7f7347b872f0e87b11d6
Aran Dekar <arandekar@Arans-MacBook-Pro.local>
2017-10-10 16:39:21 +1100

    deleted app folder (redundant)

--- api/api/proxies/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const kafka = require("./kafka");
-exports.kafka = kafka;
-const http = require("./http");
-exports.http = http;
-//# sourceMappingURL=index.js.map
